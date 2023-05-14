c12ada7ad7e07ab6552cdcbe0e27141ca0043f94
eonesty <eonesty@naver.com>
2023-05-14 01:06:39 -0400

    fixed it.

--- assessGrade.cpp
+++ assessGrade.cpp
@@ -13,9 +13,9 @@
 #pragma warning(disable: 4996)
 
 
-void assessGrade(char* letterGrade)
+void assessGrade(const char* letterGrade)
 {
-    char grade[3];
+    char grade[4] = { 0 };
     int len = strlen(letterGrade);
 
     if (len == 2 && letterGrade[1] == '+')
@@ -24,70 +24,100 @@ void assessGrade(char* letterGrade)
         grade[1] = '+';
         grade[2] = '\0';
     }
+    else if (len == 3)
+    {
+        grade[0] = toupper(letterGrade[0]);
+        grade[1] = toupper(letterGrade[1]);
+        grade[2] = toupper(letterGrade[2]);
+        grade[3] = '\0';
+    }
     else
     {
         grade[0] = toupper(letterGrade[0]);
         grade[1] = '\0';
     }
-
-    switch (grade[0])
+    printf("len :  %d grade :   %s", len, grade[1]);
+    if (len == 1)
     {
-    case 'A':
-        if (grade[1] == '+')
+        switch (grade[0])
         {
-            printf("Student achieved 95.00 %% which is a PASS condition.\n");
+              case 'A':
+                 if (grade[1] == '+')
+                 {
+                     printf("Student achieved 95.00 %% which is a PASS condition.\n");
+                 }
+                 else
+                 {
+                      printf("Student achieved 85.00 %% which is a PASS condition.\n");
+                 }
+                break;
+        case 'B':
+            if (grade[1] == '+')
+            {
+                printf("Student achieved 77.00 %% which is a PASS condition.\n");
+            }
+            else
+            {
+                printf("Student achieved 72.00 %% which is a PASS condition.\n");
+            }
+            break;
+        case 'C':
+            if (grade[1] == '+')
+            {
+                printf("Student achieved 67.00 %% which is a PASS condition.\n");
+            }
+            else
+            {
+                printf("Student achieved 62.00 %% which is a PASS condition.\n");
+            }
+            break;
+        case 'D':
+            printf("Student achieved 57.00 %% which is a PASS condition.\n");
+            break;
+        case 'F':
+            printf("Student achieved 50.00 %% which is a FAIL condition.\n");
+            break;
+        case 'I':
+            printf("Student has Special Situation : I (Incomplete)\n");
+            break;
+        case 'Q':
+            printf("Student has Special Situation : Q (Withdrawal After Drop/Refund Date)\n");
+            break;
+        default:
+            printf("¡°**ERROR : Invalid Input\n");
+            break;
         }
-        else
+    }
+    else if (len == 2)
+    {
+        switch (grade[0])
         {
-            printf("Student achieved 85.00 %% which is a PASS condition.\n");
+        case 'A':
+            if (grade[1] == 'U')
+                printf("Student has Special Situation : AU (Audit Condition)\n");
+                break;
         }
-        break;
-    case 'B':
-        if (grade[1] == '+')
+    }
+    else if ( len == 3 )
+    {
+        switch (grade[0])
         {
-            printf("Student achieved 77.00 %% which is a PASS condition.\n");
+        case 'D':
+            if (grade[1] == 'N' && grade[2] == 'A')
+            printf("Student has Special Situation : DNA (Did Not Attend)\n");
+            break;
+        case 'I':
+            if (grade[1] == '/' && grade[2] == 'P')
+            printf("Student has Special Situation : I/P (In Process)\n");
+            break;
+        default:
+            printf("¡°**ERROR : Invalid Input\n");
+            break;
         }
-        else
-        {
-            printf("Student achieved 72.00 %% which is a PASS condition.\n");
-        }
-        break;
-    case 'C':
-        if (grade[1] == '+')
-        {
-            printf("Student achieved 67.00 %% which is a PASS condition.\n");
-        }
-        else
-        {
-            printf("Student achieved 62.00 %% which is a PASS condition.\n");
-        }
-        break;
-    case 'D':
-        printf("Student achieved 57.00 %% which is a PASS condition.\n");
-        break;
-    case 'F':
-        printf("Student achieved 50.00 %% which is a FAIL condition.\n");
-        break;
-    case 'I':
-        printf("Student has Special Situation : I (Incomplete)\n");
-        break;
-    case 'Q':
-        printf("Student has Special Situation : Q (Withdrawal After Drop/Refund Date)\n");
-        break;
-    case 'AU':
-        printf("Student has Special Situation : AU (Audit Condition)\n");
-        break;
-    case 'DNA':
-        printf("Student has Special Situation : DNA (Did Not Attend)\n");
-        break;
-    case 'I/P':
-        printf("Student has Special Situation : I/P (In Process)\n");
-        break;
-    default:
-        printf("¡°**ERROR : Invalid Input\n");
-        break;
+
     }
 }
+
 
 
 void assessGrade(double doubleGrade)
@@ -168,8 +198,4 @@ void parseUserInput(char* input)
         }
         assessGrade(arrInputs);
     }
-
-
-
-}
-
+ }
